8c441d5604bb582b40dff7163ac5e8ffaf2cc83e
Tyler Knapp <tgknapp11@gmail.com>
2019-09-09 15:24:04 -0400

    bg image

--- src/ball.js
+++ src/ball.js
@@ -1,6 +1,6 @@
 // document.addEventListener("DOMContentLoaded", () => {
 //   const bg = document.getElementById("game-screen");
-//   let target = `<div style="position: relative;"><link style="position: relative;" href="#" class="target-link"><img src="assets/images/target.svg" class="target" alt=""></link href="#"></div>`;
+//   let target = `<div style="position: relative;"><link style="position: relative;" href="#" class="target-link"><img style="height: 75px; width: auto;" src="http://plug.nyc/wp-content/uploads/2019/09/target.png" class="target" alt=""></link href="#"></div>`;
 
 //   bg.insertAdjacentHTML("afterbegin", target);
 // });
