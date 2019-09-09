bfeb46dec337948cf84901aa290659bf0b8410a1
Tyler Knapp <tgknapp11@gmail.com>
2019-09-09 15:14:00 -0400

    eh

---
+++ src/ball.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const bg = document.getElementById("game-screen");
+//   let target = `<div style="position: relative;"><link style="position: relative;" href="#" class="target-link"><img src="assets/images/target.svg" class="target" alt=""></link href="#"></div>`;
+
+//   bg.insertAdjacentHTML("afterbegin", target);
+// });
